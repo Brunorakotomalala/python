d8142320372e4af75ff7847a5e92e6c289244524
Application <133837591+Brunorakotomalala@users.noreply.github.com>
2023-08-11 19:29:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,73 @@
-from flask import Flask
+from flask import Flask, request
+import openai
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# Clé d'accès API OpenAI
+openai.api_key = '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'
 
-if __name__ == '__main__':
-    app.run()
+# Clés d'accès Messenger
+PAGE_ACCESS_TOKEN = 'EAARgJ3oIAXgBOxLcGuMK6gNvAa4E93vXZAMNjG1MQWN0zUW9PnjmWNCG8CsrhN2tQlZBE0kwfW89nvgSoZCtT1VzoUK7QdRrZCTefRnmKSAcBtRtbUZCRomJOXrXHThQdSxsjdZCVmkRRjoDbhN9lnNn4KBrL4CeQjX62lBor5A6CF0mpln01gM0lKRDZAx'
+VERIFY_TOKEN = 'Moramanga'
+
+# Fonction pour obtenir une réponse de OpenAI
+def conversation_openai(prompt):
+    response = openai.Completion.create(
+        engine='davinci',
+        prompt=prompt,
+        max_tokens=150
+    )
+    return response.choices[0].text.strip()
+
+# Fonction pour envoyer un message via Messenger
+def send_message(recipient_id, message_text):
+    import requests
+
+    endpoint = 'https://graph.facebook.com/v12.0/me/messages'
+    
+    params = {
+        'access_token': PAGE_ACCESS_TOKEN
+    }
+
+    data = {
+        'recipient': {
+            'id': recipient_id
+        },
+        'message': {
+            'text': message_text
+        }
+    }
+
+    response = requests.post(endpoint, params=params, json=data)
+    if response.status_code == 200:
+        print("Message envoyé avec succès")
+    else:
+        print("Échec de l'envoi du message")
+
+# Route pour vérifier le Webhook
+@app.route('/', methods=['GET'])
+def verify_webhook():
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.verify_token") == VERIFY_TOKEN:
+        return request.args.get("hub.challenge"), 200
+    return "Invalid verification token", 403
+
+# Route pour recevoir les messages
+@app.route('/', methods=['POST'])
+def receive_message():
+    data = request.get_json()
+
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            for messaging_event in entry['messaging']:
+                if messaging_event.get('message'):
+                    sender_id = messaging_event['sender']['id']
+                    message_text = messaging_event['message']['text']
+                    response_text = conversation_openai(message_text)
+                    send_message(sender_id, response_text)
+    
+    return "Message Received", 200
+
+if __name__ == "__main__":
+    app.run(port=3000)
+
 
